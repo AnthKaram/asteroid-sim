b2504cb62b771d944bae427f7b7a64f8fccf0a16
freshbair <chrissabbagh999@gmail.com>
2025-10-04 13:38:07 +0300

    Reapply "added kinetic energy"
    This reverts commit 1ac7b27a8aff0b2a83df76e12123b83f83c8a3df.

--- app.py
+++ app.py
@@ -1,11 +1,18 @@
 # app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+def calculate_kinetic_energy(mass, velocity):
+    return 0.5 * mass * velocity ** 2
+
 @app.route("/")
 def index():
-    return render_template("index.html")
+    # Example values for mass and velocity
+    mass = 1000  # in kilograms
+    velocity = 20  # in meters per second
+    kinetic_energy = calculate_kinetic_energy(mass, velocity)
+    return render_template("index.html", kinetic_energy=kinetic_energy)
 
 if __name__ == "__main__":
     app.run(debug=True)
